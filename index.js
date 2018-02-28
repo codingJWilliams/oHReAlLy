ad5d5c03d08bfc7cfa6d8524e56eb210bffee120
Jay Williams <course@jaywilliams.me>
2018-02-28 17:53:45 +0000

    ok now?

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const creds = require("./creds.json")
 var Imgflipper = require("imgflipper");
 var http = require("http")
 var imgflipper = new Imgflipper(creds.imgflip, creds.pw);
-// Create simple echo bot 
 var lastMessage = "";
 
 login({ email: creds.email, password: creds.pw }, (err, api) => {
     if (err) return console.error(err);
-
     api.listen((err, message) => {
         if (message.threadID !== "1813947025313467") return;
         var text = message.body;
@@ -24,6 +22,6 @@ login({ email: creds.email, password: creds.pw }, (err, api) => {
                 }, message.threadID)
             });
         }
-        imgflipper.generateMeme(102156234, "", lastMessage.substring(0, 200), cb);
+        imgflipper.generateMeme(102156234, "", lastMessage.substring(0, 200).replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g, ''), cb);
     });
 });
